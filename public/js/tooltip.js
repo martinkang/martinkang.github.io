c35c3fa33a31c9ffad56ef0d627105581db9315d
martinkang <hiyou63@naver.com>
2022-01-21 02:10:49 +0900

    tooltip 사이즈와 위치 변경
    tooltip 사이즈와 위치 변경 임시 커밋.
    정리 필요

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -1,4 +1,67 @@
 
+let gWindowXSize = window.innerWidth;
+let gWindowYSize = window.innerHeight;
+
+function getWindowSize()
+{
+  gWindowXSize = window.innerWidth;
+  gWindowYSize = window.innerHeight;
+}
+
+function setToolTipSizeNPosition( aToolTip )
+{
+  let sSideBarWidth = document.getElementsByClassName( 'sidebar' ).item(0).offsetWidth;
+  let sToolTipTop = aToolTip.getElementsByClassName( 'tooltip-top' ).item(0);
+  let sToolTipXPositon = aToolTip.offsetLeft;
+  let sToolTipTopWidth = sToolTipTop.offsetWidth;
+  let sSizeDiff = '';
+  let sSize = '';
+
+  if ( gWindowXSize <= 600 ) {
+    sSideBarWidth = 0;
+  }
+
+  if ( sToolTipXPositon + sToolTipTopWidth > (gWindowXSize - sSideBarWidth) * 0.9 ) {
+    let sOriginSize = sToolTipTopWidth;
+    let sChangeSize = (gWindowXSize - sSideBarWidth) * 0.7;
+
+    sSizeDiff = ( (gWindowXSize - sSideBarWidth) * 0.9 - (sToolTipXPositon + sToolTipTopWidth) ).toString() + 'px';
+
+    if ( sOriginSize > sChangeSize ) {
+      sSize = sChangeSize.toString() + 'px';
+      sSizeDiff = ( (gWindowXSize - sSideBarWidth) * 0.9 - (sToolTipXPositon + sChangeSize) ).toString() + 'px';
+    }
+    else {
+      sSize = sOriginSize.toString() + 'px';
+      sSizeDiff = ( (gWindowXSize - sSideBarWidth) * 0.9 - (sToolTipXPositon + sOriginSize) ).toString() + 'px';
+    }
+  }
+  else {
+    sSize = "max-content";
+    sSizeDiff = '20px';
+  }
+  
+  sToolTipTop.style.width = sSize;
+  sToolTipTop.style.left = sSizeDiff;
+}
+
+function getToolTipNo( aToolTipID )
+{
+    let sToolTipNo = document.createElement('span');
+
+    sToolTipNo.innerHTML = '<span class="tooltip-no">[' + aToolTipID + ']</span>';
+    return sToolTipNo;
+}
+
+function getToolTipDesc( aDesc )
+{ 
+  let sToolTipDesc = document.createElement('span');
+  sToolTipDesc.innerHTML = '<span class="tooltiptext tooltip-top">' +
+  aDesc + '</span>';
+  
+  return sToolTipDesc;
+}
+
 function setFootNotes()
 {
     sToolTips = document.getElementsByClassName( 'tooltip' );
@@ -8,21 +71,26 @@ function setFootNotes()
         let sToolTipID = sToolTip.id.substr(3).toString();
         let sDesc = document.getElementById( 'desc-' + sToolTipID );
 
-        let sToolTipNo = document.createElement('span');
-        let sToolTipDesc = document.createElement('span');
-
-        sToolTipNo.innerHTML = '<span class="tooltip-no">[' + sToolTipID + ']</span>';
-        sToolTip.appendChild(sToolTipNo);
-        
-        sToolTipDesc.innerHTML = '<span class="tooltiptext tooltip-top">[' + sToolTipID + '] ' + 
-                                sDesc.textContent + '</span>';
-                        
-        sToolTip.appendChild( sToolTipDesc );
         sDesc.textContent = '[' + sToolTipID + '] ' + sDesc.textContent;
+        sToolTip.appendChild( getToolTipNo( sToolTipID ) );
+        sToolTip.appendChild( getToolTipDesc( sDesc.textContent ) );
+
+        setToolTipSizeNPosition( sToolTip );
     }
 }
 
-window.addEventListener( "pageshow", (event) => 
-{
-  setFootNotes();
+window.addEventListener( "resize", (event) => {
+  let sToolTips = document.getElementsByClassName( 'tooltip' );
+
+  getWindowSize();
+
+  for (let i = 0; i < sToolTips.length; i++) {
+    let sToolTip = sToolTips.item(i);
+    setToolTipSizeNPosition( sToolTip );
+  }
 });
+
+window.onload = function() {
+  getWindowSize();
+  setFootNotes();
+}
